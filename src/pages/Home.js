8d7a03e88a58dad8f1110847a19e79dcd6d8b2d1
tanvirgeek <tanvirgeek@gmail.comm>
2019-09-16 17:02:08 +0600

    Week4 Day three work done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Hero from "../components/Hero.js";
+import Banner from "../components/Banner.js";
+import {Link} from "react-router-dom";
 const Home = () => {
 	return (
-		<Hero></Hero>
+		<Hero>
+			<Banner title="Tanvir is Cool" subtitle="Delux rooms starting at $299">
+				<Link to="/rooms" className="btn-primary">
+					Our Rooms
+				</Link>
+			</Banner>
+		</Hero>
 	)
 }
 
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Hero from '../components/Hero.js';
+import Banner from "../components/Banner.js";
+import {Link} from "react-router-dom";
 
 const Rooms = () => {
 	return (
-		<Hero hero="roomsHero"/>
+		<Hero hero="roomsHero">
+			<Banner title="Our Rooms" subtitle="Delux rooms starting at $299">
+				<Link to="/" className="btn-primary">
+					Home
+				</Link>
+			</Banner>
+		</Hero>
 	)
 }
 
